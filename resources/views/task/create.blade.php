8e360d61dfadd70adffc2fe771dc1263742dc9b7
Eli <eliliao1990@gmail.com>
2017-08-24 22:14:43 +0800

    把blade內有重複的功能抽取出來

--- resources/views/task/create.blade.php
+++ resources/views/task/create.blade.php
@@ -1,15 +1,31 @@
-<h1>新增家事表單</h1>
+<html>
+<head>
+ @include('task.head')
+<title></title>
+  <!-- Boostrap minified CSS -->
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 
-<form method="POST" action="/task" >  
-	{{ csrf_field() }}  {{-- 安全機制 token碼 --}}
+</head>
+<body>
+@include('task.navbar')
+<div class="container">
+  	 <h1 class='page-header'>新增家事表單</h1>
+  		<form method="POST" action="/task" >  
+			{{ csrf_field() }}  {{-- 安全機制 token碼 --}}
 
-	<h4>名稱：</h4>
-	<input type="text" name="name">
-	
-	<h4>敘述:</h4>
-	<input type="text" name="description">
+			<h4>名稱：</h4>
+			<input type="text" name="name" class="form-control">
+		
+			<h4>敘述:</h4>
+			<input type="text" name="description" class="form-control">
+			<br>
+			<br>
+			<input class="btn btn-primary" type="submit" value="送出">
+	</form>
+   
+    <br>
+    <br>
+@include('task.scripts')
+</body>
 
-	<br>
-	<br>
-	<input type="submit" value="送出">
-</form>
+</html>
--- resources/views/task/index.blade.php
+++ resources/views/task/index.blade.php
@@ -1,57 +1,15 @@
 <!doctype html>
 <html>
-
 <head>
-
-  <!-- Boostrap minified CSS -->
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-
-  <!-- fontawesome minified CSS --> 
-  <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
-
+ @include('task.head')
 </head>
 
 <body>
 
-  <nav class="navbar navbar-default">
-    <div class="container-fluid">
-      <!-- Brand and toggle get grouped for better mobile display -->
-      <div class="navbar-header">
-        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-          <span class="sr-only">Toggle navigation</span>
-          <span class="icon-bar"></span>
-          <span class="icon-bar"></span>
-          <span class="icon-bar"></span>
-        </button>
-        <a class="navbar-brand" href="#">Stream Workshop</a>
-      </div>
-
-      <!-- Collect the nav links, forms, and other content for toggling -->
-      <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-        <ul class="nav navbar-nav">
-          <li class="active"><a href="/task">家事管理 <span class="sr-only">(current)</span></a></li>
-          <li><a href="/articles">文章管理</a></li>
-        </ul>
-        <ul class="nav navbar-nav navbar-right">
-          <li class="dropdown">
-            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
-            <img src="https://scontent.ftpe8-3.fna.fbcdn.net/v/t1.0-9/310161_312243918791479_516709523_n.jpg?oh=86235d5a1cabe59a57cffa3f27421d91&oe=5A29650C" style="height:  20px;width: 20px" class="img-circle">
-            Eli <span class="caret"></span></a>
-            <ul class="dropdown-menu">
-              <li><a href="#">Action</a></li>
-              <li><a href="#">Another action</a></li>
-              <li><a href="#">Something else here</a></li>
-              <li role="separator" class="divider"></li>
-              <li><a href="#">Separated link</a></li>
-            </ul>
-          </li>
-        </ul>
-      </div><!-- /.navbar-collapse -->
-    </div><!-- /.container-fluid -->
-  </nav>
+@include('task.navbar')
 
   <div class="container">
-    <h1 class='page-header'> 家事管理, {{ $name }} </h1>
+    <h1 class='page-header'> 家事管理,</h1>
     <p></p>
 
     <a href="/task/create" class="btn btn-danger">
@@ -118,13 +76,6 @@
         @endforeach
       </ul>
     </div>
-
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-
-    <!-- Latest compiled and minified JavaScript -->
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-
-
-
+@include('task.scripts')
   </body>
 </html>
